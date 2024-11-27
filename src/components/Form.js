86e8d145e64d1747dac86e47220cd7e10708f775
zhekai3322 <23024477@myrp.edu.sg>
2024-11-27 16:15:01 +0800

    Final

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,32 +1,3 @@
-// import React, { useState } from "react";
-
-// export default function Form({ onAddTask }) {
-//   const [description, setDescription] = useState("");
-
-//   const handleSubmit = (event) => {
-//     // TODO: write code to handle form submission
-//     event.preventDefault();
-//     if (description) {
-//       onAddTask(description);
-//       setDescription("");
-//     }
-//   };
-
-//   return (
-//     /*TODO: add a form to add a new task*/
-//     <form onSubmit={handleSubmit}>
-//       <input
-//         type="text"
-//         value={description}
-//         onChange={(e) => setDescription(e.target.value)}
-//         placeholder="Enter task description"
-//       />
-//       <button type="submit">Add Task</button>
-//     </form>
-//   );
-// }
-
-
 import React, { useState } from "react";
 
 export default function Form({ onAddTask }) {
