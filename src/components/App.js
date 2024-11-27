eab6cf5d00e5d3bdba66934321640ae6ec0ff951
zhekai3322 <23024477@myrp.edu.sg>
2024-11-27 16:04:57 +0800

    6th

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,47 @@
+// import React, { useState } from "react";
+// import Form from "./Form";
+// import TaskList from "./TaskList";
+
+// function App() {
+//   const [tasks, setTasks] = useState([]);
+
+//   const handleAddTask = (newTaskDescription) => {
+//     // TODO: write code to add a new task
+//     const newTask = {
+//       id: Date.now(),
+//       description: newTaskDescription,
+//       completed: false,
+//     };
+//     setTasks([...tasks, newTask]); 
+//   };
+
+//   const handleToggleTask = (id) => {
+//     // TODO: write code to toggle a task's status
+//     const updatedTasks = tasks.map((task) =>
+//       task.id === id ? { ...task, completed: !task.completed } : task
+//     );
+//     setTasks(updatedTasks);
+//   };
+
+//   const handleDeleteTask = (id) => {
+//     // TODO: write code to delete a task
+//     const updatedTasks = tasks.filter((task) => task.id !== id);
+//     setTasks(updatedTasks);
+//   };
+
+//   return (
+//     <div className="app">
+//       <h1>Task Tracker</h1>
+//       {/*TODO: add a form to add a new task*/}
+//       {/*TODO: add a list of tasks*/}
+//       <Form onAddTask={handleAddTask} />
+//       <TaskList tasks={tasks} onToggleTask={handleToggleTask} onDeleteTask={handleDeleteTask} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 import Form from "./Form";
 import TaskList from "./TaskList";
@@ -6,17 +50,15 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const handleAddTask = (newTaskDescription) => {
-    // TODO: write code to add a new task
     const newTask = {
       id: Date.now(),
       description: newTaskDescription,
       completed: false,
     };
-    setTasks([...tasks, newTask]); 
+    setTasks([...tasks, newTask]);
   };
 
   const handleToggleTask = (id) => {
-    // TODO: write code to toggle a task's status
     const updatedTasks = tasks.map((task) =>
       task.id === id ? { ...task, completed: !task.completed } : task
     );
@@ -24,7 +66,6 @@ function App() {
   };
 
   const handleDeleteTask = (id) => {
-    // TODO: write code to delete a task
     const updatedTasks = tasks.filter((task) => task.id !== id);
     setTasks(updatedTasks);
   };
@@ -32,8 +73,6 @@ function App() {
   return (
     <div className="app">
       <h1>Task Tracker</h1>
-      {/*TODO: add a form to add a new task*/}
-      {/*TODO: add a list of tasks*/}
       <Form onAddTask={handleAddTask} />
       <TaskList tasks={tasks} onToggleTask={handleToggleTask} onDeleteTask={handleDeleteTask} />
     </div>
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,36 @@
+// import React from "react";
+
+// // /**
+// //  * TODO: implement the Task component
+// //  * @function Task
+// //  * @returns {React.Component} A <li> containing the task description and a checkbox to toggle completion
+// //  */
+// export default function Task({ task, onToggleTask, onDeleteTask }) {
+//   const handleToggle = () => {
+//     onToggleTask(task.id);
+//   };
+
+//   const handleDelete = () => {
+//     onDeleteTask(task.id);
+//   };
+
+//   return (
+//     <li>
+//       <input
+//         type="checkbox"
+//         checked={task.completed}
+//         onChange={handleToggle}
+//       />
+//       <span style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+//         {task.description}
+//       </span>
+//       <button onClick={handleDelete}>Delete</button>
+//     </li>
+//   );
+// }
+
 import React from "react";
 
-// /**
-//  * TODO: implement the Task component
-//  * @function Task
-//  * @returns {React.Component} A <li> containing the task description and a checkbox to toggle completion
-//  */
 export default function Task({ task, onToggleTask, onDeleteTask }) {
   const handleToggle = () => {
     onToggleTask(task.id);
